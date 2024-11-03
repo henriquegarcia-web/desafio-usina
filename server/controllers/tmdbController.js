dde6d466743d686475e44ef6fd5748b4c4cd9edd
Henrique Garcia <henriquegarcia.dev@gmail.com>
2024-11-03 15:22:58 -0300

    fix(backend): Ajuste na URL padrão da API do TMDB

--- server/controllers/tmdbController.js
+++ server/controllers/tmdbController.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch')
 
 const TMDB_API_URL =
-  'https://api.themoviedb.org/3/search/multi?include_adult=false&language=en-US&page=1'
+  'https://api.themoviedb.org/3/search/movie?include_adult=false&language=pt-BR&page=1'
 const TMDB_API_KEY =
   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMmYzMmI0YzM3YzUwYTdkYjA0YjlkYzRjODQ5ZDUyMSIsIm5iZiI6MTczMDY1MzU0My4xMzMyMTcsInN1YiI6IjY3MjdhYzQ3YzBiYzA3NDlkMGQ4YTkyYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ck7qXihv4UwIWpGK-6ul3X5RGZJJYRzOgIitmgZYIZA'
 
